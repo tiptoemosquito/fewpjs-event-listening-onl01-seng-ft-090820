074a1bd0ecdfc69c2f7404e756e823d69b1208b9
tiptoemosquito <clhoward33@icloud.com>
2020-12-09 14:19:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-function addingEventListener() {
+
+
+function addingEventListener() { 
+    const input = document.getElementById('input');
+    input.addEventListener('click', function(event) {
+        alert('I was clicked!');
+    })
 }
+
+// addingEventListener()
